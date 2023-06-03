11f9d2e617a619ff4f07653bae349c01e18bb8a3
PrimeEagle <misc@hiddendragon.com>
2023-06-03 19:03:01 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Mathematics@1129 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.Mathematics/Filter/FilterParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace VNet.Mathematics.Filter;
+
+public class FilterParameters : IFirFilterArgs, IIirFilterParameters, ILowPassFilterArgs, IHighPassFilterArgs, IBandPassFilterArgs, INotchFilterArgs
+{
+    public double SampleRate { get; set; }
+    public double CutoffFrequency { get; set; }
+    public double Bandwidth { get; set; }
+    public BandType BandType { get; set; }
+    public int Order { get; set; }
+    public WindowFunction WindowFunction { get; set; }
+    public double Sigma { get; set; }
+    public FilterType FilterType { get; set; }
+    public double LowCutoffFrequency { get; set; }
+    public double HighCutoffFrequency { get; set; }
+}
